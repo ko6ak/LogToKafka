4656d28636c1eff7a47878645eba687130b7628d
Den <ko6ak@ya.ru>
2024-03-18 14:08:18 +0300

    Initial commit

---
+++ src/main/java/org/project/logtokafka/LogToKafkaApplication.java
@@ -0,0 +1,13 @@
+package org.project.logtokafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogToKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogToKafkaApplication.class, args);
+    }
+
+}
